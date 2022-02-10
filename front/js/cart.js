dd63ca0cbf1d1c6995e9a54e238a0a6c8feb4115
David Decottigny <ddecottigny@gmail.com>
2022-02-10 09:58:23 +0100

    panier affiché avec valeurs récupérées via fetch API

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,36 +6,53 @@ console.log(productsCheck);
 const cartSelector = document.querySelector("#cart__items");
 console.log(cartSelector);
 
-// Si le panier est vide :
-if (productsCheck == null) {
-  cartSelector.innerHTML = `<p>Votre panier est vide</p>`;
-} else {
-  //   console.log("Le panier n'est pas vide");
+const getProduct = async (productId) => {
+  await fetch(`http://localhost:3000/api/products/${productId}`)
+    .then((res) => res.json())
+    .then((response) => {
+      product = response;
+      // console.log(product);
+    })
+    // Alerte en cas d'erreur de chargement de l'API
+    .catch((error) => {
+      product = false;
+      alert("Impossible de charger les informations de ce produit");
+    });
+};
 
-  // Création d'un tableau support pour accueillir les tours de boucle du localStorage
-  let cartProductsSupport = [];
+// Mise en place de l'affichage des produits du panier
+const displayProductCart = async () => {
+  // Si le panier est vide :
+  if (productsCheck == null) {
+    cartSelector.innerHTML = `<p>Votre panier est vide</p>`;
+  } else {
+    //   console.log("Le panier n'est pas vide");
 
-  // Incrémentation en boucle des infos du localStorage
-  for (i = 0; i < productsCheck.length; i++) {
-    // console.log(productsCheck.length);
-    cartProductsSupport =
-      cartProductsSupport +
-      `<article
+    // Création d'un tableau support pour accueillir les tours de boucle du localStorage
+    let cartProductsSupport = [];
+
+    // Incrémentation en boucle des infos du localStorage
+    for (i = 0; i < productsCheck.length; i++) {
+      // console.log(productsCheck.length);
+      await getProduct(productsCheck[i].id);
+      cartProductsSupport =
+        cartProductsSupport +
+        `<article
                 class="cart__item"
                 data-id="${productsCheck[i].id}"
                 data-color="${productsCheck[i].colors}"
               >
                 <div class="cart__item__img">
                   <img
-                    src="${productsCheck[i].imageUrl}"
-                    alt="${productsCheck[i].altTxt}"
+                    src="${product.imageUrl}"
+                    alt="${product.altTxt}"
                   />
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
-                    <h2>${productsCheck[i].name}</h2>
+                    <h2>${product.name}</h2>
                     <p>${productsCheck[i].colors}</p>
-                    <p>${productsCheck[i].price}</p>
+                    <p>${product.price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
@@ -55,7 +72,10 @@ if (productsCheck == null) {
                   </div>
                 </div>
               </article>`;
+    }
+    // Injection du tableau dans le DOM
+    cartSelector.innerHTML = cartProductsSupport;
   }
-  // Injection du tableau dans le DOM
-  cartSelector.innerHTML = cartProductsSupport;
-}
+};
+// Appel de la fonction pour afficher le panier
+displayProductCart();
