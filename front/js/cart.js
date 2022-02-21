7ec72c7cae97ce96f2053091ca7abc161828ebcc
David Decottigny <ddecottigny@gmail.com>
2022-02-21 10:25:07 +0100

    Optimisation du code de calcul de la quantité totale du panier et du montant total du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -76,26 +76,30 @@ const displayProductCart = async () => {
     // console.log(product.price);
     // Injection du tableau dans le DOM
     cartSelector.innerHTML = cartProductsSupport;
+    // appel de la fonction afin de récupérer les valeurs des input
+    quantityUpdate();
   }
 };
 // Appel de la fonction pour afficher le panier
 displayProductCart();
 
 // Gestion du nombre total d'articles dans le panier et montant total du panier
+
 const totalProductsQuantity = () => {
+  // Ciblage dans le DOM pour afficher la quantité totale de produits dans le panier
   const totalQuantitySelector = document.querySelector("#totalQuantity");
   // console.log(totalQuantitySelector);
 
-  // Création d'un tableau pour accueillir les quantités des produits du panier
-  let totalQuantity = [];
-
-  // Je crée une boucle pour recueillir les quantités dans le localStorage et je les push dans le tableau
+  // Initialisation de la base du calcul de la quantité totale du panier
+  let quantity = 0;
+  // Je crée une boucle pour recueillir les quantités dans le localStorage et j'additionne les quantités croisées à chaque tour de boucle
   productsCheck.forEach((cartProduct) => {
-    totalQuantity.push(cartProduct.quantity);
+    quantity += cartProduct.quantity;
   });
 
-  // Addition des quantités et insertion du résultat dans le DOM
-  totalQuantitySelector.innerText = eval(totalQuantity.join("+"));
+  // Insertion du résultat dans le DOM
+
+  totalQuantitySelector.innerText = quantity;
 };
 
 // Appel de la fonction calculant le total d'articles dans le panier
@@ -104,21 +108,27 @@ totalProductsQuantity();
 // Gestion du prix total du panier
 
 const totalProductsPrice = async () => {
+  // Ciblage dans le DOM pour afficher le montant total du panier
   const totalPriceSelector = document.querySelector("#totalPrice");
   // console.log(totalPriceSelector);
 
-  // Création d'un tableau pour accueillir les prix des produits du panier
-  let totalPrice = [];
+  // Initialisation de la base du calcul du prix total
+  let totalPrice = 0;
 
-  // Je crée une boucle pour recueillir les prix dans le localStorage et à l'aide de l'API, puis je les push dans le tableau
-  for (j = 0; j < productsCheck.length; j++) {
-    await getProduct(productsCheck[j].id);
-
-    totalPrice.push(product.price);
+  // Je crée une boucle pour recueillir les prix dans le localStorage et à l'aide de l'API, puis je les cumule
+  for (var i = 0; i < productsCheck.length; i++) {
+    await getProduct(productsCheck[i].id);
+    totalPrice += productsCheck[i].quantity * product.price;
+    // totalPrice.push(product.price);
   }
-  console.log(totalPrice);
+  // console.log(totalPrice);
 
-  // Addition des quantités et insertion du résultat dans le DOM
-  totalPriceSelector.innerText = eval(totalPrice.join("+"));
+  // Insertion du résultat dans le DOM
+  totalPriceSelector.innerText = totalPrice;
 };
 totalProductsPrice();
+
+// Gestion de la mise à jour des quantités d'articles sur la page panier
+
+// Fonction validant la mise à jour des quantités dans le panier
+const quantityUpdate = () => {};
